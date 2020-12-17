a98244a463767621bb0e36df35a930dc5eefee6f
George Cope <G.Cope13@gmail.com>
2020-12-17 13:07:21 +0000

    Created a function called promptUser to collect user inputs and call a generatePass function

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+// User prompts to gather info on what kind of password they'd like
+// Calls the generatePass function at the end
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+function promptUser() {
+  var length = prompt("How long would you like your password to be? Between 8 and 128 characters.");
+  var lower = confirm("Would you like lower case letters?");
+  var upper = confirm("Would you like upper case letters?"); 
+  var num = confirm("Would you like numbers?");
+  var sym = confirm("Would you like symbols?");
 
-  passwordText.value = password;
-
+  generatePass(lower, upper, num, sym, length);
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
