a007c8cfae154372caa514421eeaa84091f6870a
George Cope <G.Cope13@gmail.com>
2021-01-05 19:20:44 +0000

    Initial commit. Changes to script to make the generator more robust

--- script.js
+++ script.js
@@ -1,41 +1,36 @@
-// function checkLength() {
-//   var length = prompt("How long would you like your password to be? Between 8 and 128 characters.");
-//   if(length < 8 || length > 128) {
-//     alert("Please select a password length between 8 and 128 characters.");
-//     checkLength();
-//   } else {
-//     promptUser();
-//   }
-// }
-
-
-// User prompts to gather info on what kind of password they'd like
-// Calls the generatePass function at the end
-
-function promptUser() {
+function checkLength() {
   var length = prompt("How long would you like your password to be? Between 8 and 128 characters.");
+  if(length < 8 || length > 128) {
+    alert("Please select a password length between 8 and 128 characters.");
+    checkLength();
+  } else {
+    promptUser(length);
+  }
+}
+
+
+
+function promptUser(length) {
+  // var length = prompt("How long would you like your password to be? Between 8 and 128 characters.");
   var lower = confirm("Would you like lower case letters?");
   var upper = confirm("Would you like upper case letters?"); 
   var num = confirm("Would you like numbers?");
   var sym = confirm("Would you like symbols?");
 
+
   generatePass(lower, upper, num, sym, length);
+  console.log("promptUser works");
 }
 
 
-// Assigns generateBtn variable to an HTML object
-// Makes it quicker to target the element
 
 var generateBtn = document.querySelector("#generate");
 
 
-// Add event listener to generate button
-// Upon clicking the promptUser function will be called
 
-generateBtn.addEventListener("click", promptUser);
+generateBtn.addEventListener("click", checkLength);
 
 
-// Create object with password characters in it
 
 var passwordChars = {
   lowerCase: "abcdefghijklmnopqrstuvwxyz",
@@ -45,15 +40,12 @@ var passwordChars = {
 };
 
 
-// Write a function to generate the password
 
 function generatePass(lower, upper, num, spclC, length) {
-  // Let keyword ensures the 2 variables declared have limited scope
-  let main = "";
+  console.log("We got this far.");
+  let availableSets = [];
   let finalPassword = "";
 
-  // Values passed into this object are used in the generatePass function
-  // The values are those declared in the passwordChars object on ln 29
   var passOptions = {
     lowerCase: lower,
     upperCase: upper,
@@ -61,36 +53,25 @@ function generatePass(lower, upper, num, spclC, length) {
     specialChars: spclC
   };
 
-  // The first part of this for loop states that i should be 0 & if i 
-  // is less than the length of all the string items contained in passOptions
-  // then iterate i 
-  for(i = 0; i < Object.keys(passOptions).length; i++) {
-    // This is a ternary operator that has been assigned to the main variable via +=
-    // If object.values(passOptions) is true then passwordChars[Object.keys(passOptions)[i]] runs
-    // Otherwise it returns an empty string
-    main += (Object.values(passOptions)[i]) ? passwordChars[Object.keys(passOptions)[i]] : "";
-  }
-  // This if statement states that if main is not empty & the length is less than 0
-  // Then do something
-  if(main != "" && length > 0) {
-    // The something mentioned above is this for loop
-    // i is set to 0 & if i is less than the input length, we increment
-    for(i = 0; i < length; i++) {
-      // As it iterates the finalPassword variable is being assigned 
-      // main[Math.floor(Math.random() * main.length)]
-      finalPassword += main[Math.floor(Math.random() * main.length)];
-    }
+  console.log(passOptions);
 
-    // The finalPassword variable is being written to the HTML element with
-    // the id of password
+  for(i = 0; i < Object.keys(passOptions).length; i++) {
+    if(Object.values(passOptions)[i] === true) {
+      availableSets.push(passwordChars[Object.keys(passOptions)[i]]);
+    }
+  }
+
+
+  if(availableSets != [] && length > 0) {
+    for(i = 0; i < length; i++) {
+      var characters = Object.values(availableSets)[i];
+      console.log(characters);
+      finalPassword += characters[Math.floor(Math.random() * )];
+      // console.log(Object.values(availableSets)[i]);
+      // finalPassword += Object.values(availableSets[Math.floor(Math.random() * length)]);
+      // finalPassword += availableSets[Math.floor(Math.random() * availableSets.length)];
+    } 
     document.querySelector("#password").value = finalPassword;
   } 
-
-  // This if statement calls back to promptUser if they declared a number <length or >length
-  // Also displays a message telling the user to enter a number between 8 and 128
-  if(length < 8 || length > 128) {
-    alert("Please enter a number from 8 - 128.");
-    promptUser();
-  }
 
 }
